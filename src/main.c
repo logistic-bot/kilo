9438adce71ee706be63d10e1cfc875097a04c3fd
Khais COLIN <logistic-bot@protonmail.com>
2021-03-14 20:51:50 +0100

    83: Display tabs correctly

--- src/main.c
+++ src/main.c
@@ -54,7 +54,9 @@ enum editorKey {
 
 typedef struct erow {
     int size;
+    int rsize;
     char* chars;
+    char* render;
 } erow;
 
 struct EditorConfig {
@@ -215,6 +217,31 @@ int get_window_size(int* rows, int* cols) {
 
 /*** row operations ***/
 
+void editor_update_row(erow* row) {
+    int tabs = 0;
+    int j;
+    for (j = 0; j < row->size; j++)
+        if (row->chars[j] == '\t')
+            tabs++;
+
+    free(row->render);
+    row->render = malloc(row->size + tabs * 3 + 1);
+
+    int idx = 0;
+    for (j = 0; j < row->size; j++) {
+        if (row->chars[j] == '\t') {
+            row->render[idx++] = ' ';
+            while (idx % 4 != 0) {
+                row->render[idx++] = ' ';
+            }
+        } else {
+            row->render[idx++] = row->chars[j];
+        }
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editor_append_row(char* s, size_t len) {
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -223,6 +250,11 @@ void editor_append_row(char* s, size_t len) {
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editor_update_row(&E.row[at]);
+
     E.numrows++;
 }
 
@@ -327,12 +359,12 @@ void editor_draw_rows(struct AppendBuffer* ab) {
             }
 
         } else {
-            int len = E.row[filerow].size - E.columnoffset;
+            int len = E.row[filerow].rsize - E.columnoffset;
             if (len < 0)
                 len = 0;
             if (len > E.screencolumns)
                 len = E.screencolumns;
-            ab_append(ab, &E.row[filerow].chars[E.columnoffset], len);
+            ab_append(ab, &E.row[filerow].render[E.columnoffset], len);
         }
 
         ab_append(ab, "\x1b[K", 3);
