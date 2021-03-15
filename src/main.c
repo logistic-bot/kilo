9be08754f4c98768b6390dae7e6baa7efae59eb3
Khais COLIN <logistic-bot@protonmail.com>
2021-03-15 13:46:41 +0100

    90: PageUp and PageDown now move the cursor correctly

--- src/main.c
+++ src/main.c
@@ -493,6 +493,14 @@ void editor_process_keypress() {
         break;
     case PAGE_UP:
     case PAGE_DOWN: {
+        if (c == PAGE_UP) {
+            E.cy = E.rowoffset;
+        } else if (c == PAGE_DOWN) {
+            E.cy = E.rowoffset - 1;
+            if (E.cy > E.numrows)
+                E.cy = E.numrows;
+        }
+
         int times = E.screenrows;
         while (times--) {
             editor_move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
