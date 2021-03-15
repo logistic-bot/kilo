46355a2cd9d3568d8130dec6635168eff1907823
Khais COLIN <logistic-bot@protonmail.com>
2021-03-15 15:07:39 +0100

    104: Block special keys

--- src/main.c
+++ src/main.c
@@ -42,6 +42,7 @@
 #define CTRL_KEY(k) ((k)&0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -547,6 +548,9 @@ void editor_move_cursor(int key) {
 void editor_process_keypress() {
     int c = editor_read_key();
     switch (c) {
+    case '\r':
+        /* TODO */
+        break;
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
@@ -558,6 +562,11 @@ void editor_process_keypress() {
     case END:
         if (E.cy < E.numrows)
             E.cx = E.row[E.cy].size;
+        break;
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL:
+        /* TODO */
         break;
     case PAGE_UP:
     case PAGE_DOWN: {
@@ -580,6 +589,9 @@ void editor_process_keypress() {
     case ARROW_RIGHT:
     case ARROW_UP:
         editor_move_cursor(c);
+        break;
+    case CTRL_KEY('l'):
+    case '\x1b':
         break;
     default:
         editor_insert_char(c);
