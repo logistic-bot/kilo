3a4a18f19f3589d07c1a5015f726b8a26e654eda
Khais COLIN <logistic-bot@protonmail.com>
2021-03-17 19:56:09 +0100

    153: Use while loop for editor_update_syntax

--- src/main.c
+++ src/main.c
@@ -273,11 +273,15 @@ void editor_update_syntax(erow* row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    int i;
-    for (i = 0; i < row->rsize; i++) {
-        if (isdigit(row->render[i])) {
+    int i = 0;
+    while (i < row->rsize) {
+        char c = row->render[i];
+
+        if (isdigit(c)) {
             row->hl[i] = HL_NUMBER;
         }
+
+        i++;
     }
 }
 
