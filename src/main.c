17b198e431d0c8099ef81db9d8312ffd1e354538
Khais COLIN <logistic-bot@protonmail.com>
2021-03-17 19:54:41 +0100

    152: Restore highlighting after search

--- src/main.c
+++ src/main.c
@@ -544,6 +544,15 @@ void editor_find_callback(char* query, int key) {
     static int last_match = -1;
     static int direction = 1;
 
+    static int saved_hl_line;
+    static char* saved_hl = NULL;
+
+    if (saved_hl) {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b') {
         last_match = -1;
         direction = 1;
@@ -581,6 +590,9 @@ void editor_find_callback(char* query, int key) {
             E.cx = editor_row_rx_to_cx(row, match - row->render);
             //            E.rowoffset = E.numrows; // TODO
 
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
             memset(&row->hl[match - row->render], HL_MATCH, strlen(query)); // highlight match
             break;
         }
