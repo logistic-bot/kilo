0af75e9ba1eb2ee6a29888a440610269751fe971
Khais COLIN <logistic-bot@protonmail.com>
2021-03-18 12:43:32 +0100

    161: Use filetype to determine whether or not to highlight some things

--- src/main.c
+++ src/main.c
@@ -300,6 +300,10 @@ void editor_update_syntax(erow* row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
+    if (E.syntax == NULL) {
+        return;
+    }
+
     int prev_sep = 1;
 
     int i = 0;
@@ -307,11 +311,13 @@ void editor_update_syntax(erow* row) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-        if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
-            row->hl[i] = HL_NUMBER;
-            i++;
-            prev_sep = 0;
-            continue;
+        if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
+            if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
+                row->hl[i] = HL_NUMBER;
+                i++;
+                prev_sep = 0;
+                continue;
+            }
         }
 
         prev_sep = is_separator(c);
