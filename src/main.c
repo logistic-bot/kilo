e05c289fdaa97a0c0d89f4ab0c167d63589ac7b4
Khais COLIN <logistic-bot@protonmail.com>
2021-03-18 12:59:53 +0100

    166: String highlight flag on c files

--- src/main.c
+++ src/main.c
@@ -64,6 +64,7 @@ enum editorHighlight {
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 
 /*** data ***/
 
@@ -108,7 +109,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
-        HL_HIGHLIGHT_NUMBERS,
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS,
     },
 };
 
